962011b82385ede05c05c78c7a313457c4667eba
oglcnarbc <oglcnarbc@oglcnarbc-2.local>
2019-11-16 20:38:33 +0300

    edits incorrect login case

--- cypress/integration/examples/trendyolLogin.spec.js
+++ cypress/integration/examples/trendyolLogin.spec.js
@@ -2,7 +2,7 @@ context('Trendyol Login', () => {
     beforeEach(() => {
         cy.visit('/')
     })
-    it('Login', () => {
+    it('Incorrect Login', () => {
         cy.SignIn('oglcnarbc@gmail.com','xyzt')
     })
 })
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -4,5 +4,7 @@ Cypress.Commands.add("SignIn", (email, password) => {
     cy.get('.icon.navigation-icon-user').click()
     cy.get('.authentication-input.email').type(email)
     cy.get('.authentication-input.password').type(password)
-    cy.get('a[id=loginSubmit]').click();
+    cy.get('a[id=loginSubmit]').click().then(()=>{
+        cy.get('.error-box').should("contain.text","Hatalı E-Posta / Şifre. Tekrar Deneyin.")
+    })
 })
