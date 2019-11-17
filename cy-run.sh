7c91786c08ab92c57786ac7ecb3c290abbc63cd9
Oğulcan Arabacı <ogulcan.a81@hotmail.com>
2019-11-17 16:50:41 +0300

    Update cy-run.sh

--- cy-run.sh
+++ cy-run.sh
@@ -1,3 +1,2 @@
-echo "Running Cypress e2e tests headlessly without copying files"
 docker run -it -v $PWD:/cypressdemo -w /cypressdemo cypress/included:3.6.0 --browser=$1 npx cypress run --spec cypress/integration/examples/$2
 
