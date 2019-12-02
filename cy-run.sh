69c467a78564a9e65f986d2cfb6ebdb5c2b0aadb
Oğulcan Arabacı <ogulcan.a81@hotmail.com>
2019-12-02 09:25:54 +0300

    Change docker workspace and mount path

--- cy-run.sh
+++ cy-run.sh
@@ -1,1 +1,1 @@
-docker run -it -v $PWD:/cypressdemo -w /cypressdemo cypress/included:3.6.0 --browser=$1 npx cypress run --spec cypress/integration/examples/$2
+docker run -it -v $PWD:/e2e -w /cypressdemo cypress/included:3.6.0 --browser=$1 npx cypress run --spec cypress/integration/examples/$2
