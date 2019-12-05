48f52577efe53d018f160bfda23b1e298be23258
Oğulcan Arabacı <ogulcan.a81@hotmail.com>
2019-12-05 21:25:15 +0300

    Update cy-run.sh

--- cy-run.sh
+++ cy-run.sh
@@ -1,1 +1,1 @@
-docker run -it -v $PWD:/e2e -w /cypressdemo cypress/included:3.6.0 --browser=$1 npx cypress run --spec cypress/integration/examples/$2
+docker run -it -v $PWD:/e2e -w /e2e cypress/included:3.6.0 --browser=$1 npx cypress run --spec cypress/integration/examples/$2
